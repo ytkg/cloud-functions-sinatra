efa287096daeb78937dbed9a39faa060c082144e
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2022-05-01 21:56:56 +0900

    functions_framework init

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 require "sinatra/base"
+require "functions_framework"
 
 class App < Sinatra::Application
   get "/" do
     "Hello world!\n"
   end
+end
+
+FunctionsFramework.http "sinatra_example" do |request|
+  App.call request.env
 end
